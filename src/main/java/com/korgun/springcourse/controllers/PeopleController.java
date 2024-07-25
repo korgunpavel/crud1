eec99642a5d1b8773e543faaf1c2c3f6ac1f83f7
Pavel Korgun <korgunpaul@gmail.com>
2024-07-25 16:21:49 +0500

    from List to JDBC API

--- src/main/java/com/korgun/springcourse/controllers/PeopleController.java
+++ src/main/java/com/korgun/springcourse/controllers/PeopleController.java
@@ -2,9 +2,11 @@ package com.korgun.springcourse.controllers;
 
 import com.korgun.springcourse.dao.PersonDAO;
 import com.korgun.springcourse.model.Person;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -35,7 +37,10 @@ public class PeopleController {
         return "people/new";
     }
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()) return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -47,7 +52,10 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
+
+        if (bindingResult.hasErrors()) return "people/edit";
+
         personDAO.update(id,person);
         return "redirect:/people";
     }
--- src/main/java/com/korgun/springcourse/model/Person.java
+++ src/main/java/com/korgun/springcourse/model/Person.java
@@ -1,18 +1,38 @@
 package com.korgun.springcourse.model;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public class Person {
     private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Count of characters should be between 2 and 30")
     private String name;
-
+    @Min(value = 0, message = "Age can`t be negative")
+    private int age;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
 
     public Person() {
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -23,11 +43,19 @@ public class Person {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
+    public int getAge() {
+        return age;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
